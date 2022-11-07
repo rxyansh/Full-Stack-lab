6080f7f0d9e94be637e30c1c2a7af8955a3750bf
rxyansh <85690923+rxyansh@users.noreply.github.com>
2022-11-06 19:47:36 -0500

    Add files via upload

---
+++ labwk9/src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1> Welcome to Full Stack Development - 1</h1>
+        </p>
+        <p>
+          <h2> 
+
+            React JS Programming Week09 Lab extercise
+
+
+
+          </h2>
+        </p>
+        <p>
+          <h3> 
+
+            101328474
+
+            
+
+
+          </h3>
+        </p>
+        <p>
+          <h3> 
+
+            Reyansh Srivastava
+
+            
+
+
+          </h3>
+        </p>
+        <p>
+          <h3>
+
+            George Brown College, Toronto 
+
+            
+
+
+          </h3>
+        </p>
+      
+      
+      
+         
+      </header>
+    </div>
+  );
+}
+
+export default App;
